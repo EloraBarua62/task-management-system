8d20c93ac1e9d0891c35f51ef7a3cbf332074b00
EloraBarua <elorabarua62@gmail.com>
2023-07-23 00:06:39 +0600

    Store of all tasks created

--- app/store.tsx
+++ app/store.tsx
@@ -1,10 +1,27 @@
 import { types } from "mobx-state-tree";
 
 
-// Model Including All Fields Of Task
+// Model including all fields of task
 export const TaskModel = types.model("TaskModel", {
     id: types.optional(types.identifier, ()=> Math.random().toString()),
     title: types.optional(types.string, "Untitles task"),
     description: types.optional(types.string, "No description"),
     status: types.optional(types.number, 0)
 });
+
+
+// Store of all task 
+export const taskStore = types.model("taskStore" , {
+    taskList: types.array(TaskModel)
+})
+
+
+
+
+// .actions(store => ({
+//     addTasks(task: any){
+//         store.taskList.push(task);
+//     },
+// }))
+
+
